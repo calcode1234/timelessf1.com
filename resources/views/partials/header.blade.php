97727214416e04c36831f33026eb3336fc04d4f5
Callum Gamble <callum@kreativeincagency.co.uk>
2022-07-05 16:59:12 +0100

    added social icons to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,6 @@
 <div id="site-header" class="section" role="header">
+    <a class="screen-reader-text skip-link btn btn-primary" href="#content">Skip to content</a>
+
     @if(has_nav_menu('primary_navigation'))
         <nav class="navbar navbar-expand-md bg-faded">
             @php wp_nav_menu(array(
@@ -47,5 +49,47 @@
         @endif
 
         @php wp_nav_menu(array( 'menu' => 'Header Menu 2' ) ) @endphp
+
+        <ul>
+            @if($tiktok)
+                <li>
+                    @php $tiktok_url  = $tiktok['url'] @endphp
+                    @php $tiktok_title  = $tiktok['title'] @endphp
+                    @php $tiktok_target = $tiktok['target'] @endphp @php '_self' @endphp
+
+                    <a href="@php echo esc_url( $tiktok_url ) @endphp" target="@php echo esc_attr( $tiktok_target ) @endphp"><i class="fab fa-tiktok"></i> <span class="sr-only">@php echo esc_html( $tiktok_title ) @endphp</span></a>
+                </li>
+            @endif
+
+            @if($youtube)
+                <li>
+                    @php $youtube_url  = $youtube['url'] @endphp
+                    @php $youtube_title  = $youtube['title'] @endphp
+                    @php $youtube_target = $youtube['target'] @endphp @php '_self' @endphp
+
+                    <a href="@php echo esc_url( $youtube_url ) @endphp" target="@php echo esc_attr( $youtube_target ) @endphp"><i class="fab fa-youtube"></i> <span class="sr-only">@php echo esc_html( $youtube_title ) @endphp</span></a>
+                </li>
+            @endif
+
+            @if($instagram)
+                <li>
+                    @php $instagram_url  = $instagram['url'] @endphp
+                    @php $instagram_title  = $instagram['title'] @endphp
+                    @php $instagram_target = $instagram['target'] @endphp @php '_self' @endphp
+
+                    <a href="@php echo esc_url( $instagram_url ) @endphp" target="@php echo esc_attr( $instagram_target ) @endphp"><i class="fab fa-instagram"></i> <span class="sr-only">@php echo esc_html( $instagram_title ) @endphp</span></a>
+                </li>
+            @endif
+
+            @if($twitter)
+                <li>
+                    @php $twitter_url  = $twitter['url'] @endphp
+                    @php $twitter_title  = $twitter['title'] @endphp
+                    @php $twitter_target = $twitter['target'] @endphp @php '_self' @endphp
+
+                    <a href="@php echo esc_url( $twitter_url ) @endphp" target="@php echo esc_attr( $twitter_target ) @endphp"><i class="fab fa-twitter"></i> <span class="sr-only">@php echo esc_html( $twitter_title ) @endphp</span></a>
+                </li>
+            @endif
+        </ul>
     </div>
 </div>
