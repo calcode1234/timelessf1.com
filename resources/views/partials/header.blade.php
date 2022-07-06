0f760c196d37838a1514b920272a28aa75dbc839
Callum Gamble <callum@kreativeincagency.co.uk>
2022-07-06 11:50:26 +0100

    social icons styling

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -36,7 +36,7 @@
           </a>
         </div>
 
-        <div class="col-3 d-lg-flex align-items-lg-center d-none">
+        <div class="col-3 d-lg-flex align-items-lg-center justify-content-lg-end d-none">
             @php wp_nav_menu(array( 'menu' => 'Header Menu 2' ) ) @endphp
         </div>
 
@@ -66,46 +66,50 @@
             @php wp_nav_menu(array( 'menu' => 'Header Menu 2' ) ) @endphp
         </div>
 
-        <ul>
-            @if($tiktok)
-                <li>
-                    @php $tiktok_url  = $tiktok['url'] @endphp
-                    @php $tiktok_title  = $tiktok['title'] @endphp
-                    @php $tiktok_target = $tiktok['target'] @endphp @php '_self' @endphp
+        <div class="row">
+          <div class="col-12">
+            <ul class="social section">
+                @if($tiktok)
+                    <li>
+                        @php $tiktok_url  = $tiktok['url'] @endphp
+                        @php $tiktok_title  = $tiktok['title'] @endphp
+                        @php $tiktok_target = $tiktok['target'] @endphp @php '_self' @endphp
 
-                    <a href="@php echo esc_url( $tiktok_url ) @endphp" target="@php echo esc_attr( $tiktok_target ) @endphp"><i class="fab fa-tiktok"></i> <span class="sr-only">@php echo esc_html( $tiktok_title ) @endphp</span></a>
-                </li>
-            @endif
+                        <a href="@php echo esc_url( $tiktok_url ) @endphp" target="@php echo esc_attr( $tiktok_target ) @endphp"><i class="fab fa-tiktok"></i> <span class="sr-only">@php echo esc_html( $tiktok_title ) @endphp</span></a>
+                    </li>
+                @endif
 
-            @if($youtube)
-                <li>
-                    @php $youtube_url  = $youtube['url'] @endphp
-                    @php $youtube_title  = $youtube['title'] @endphp
-                    @php $youtube_target = $youtube['target'] @endphp @php '_self' @endphp
+                @if($youtube)
+                    <li>
+                        @php $youtube_url  = $youtube['url'] @endphp
+                        @php $youtube_title  = $youtube['title'] @endphp
+                        @php $youtube_target = $youtube['target'] @endphp @php '_self' @endphp
 
-                    <a href="@php echo esc_url( $youtube_url ) @endphp" target="@php echo esc_attr( $youtube_target ) @endphp"><i class="fab fa-youtube"></i> <span class="sr-only">@php echo esc_html( $youtube_title ) @endphp</span></a>
-                </li>
-            @endif
+                        <a href="@php echo esc_url( $youtube_url ) @endphp" target="@php echo esc_attr( $youtube_target ) @endphp"><i class="fab fa-youtube"></i> <span class="sr-only">@php echo esc_html( $youtube_title ) @endphp</span></a>
+                    </li>
+                @endif
 
-            @if($instagram)
-                <li>
-                    @php $instagram_url  = $instagram['url'] @endphp
-                    @php $instagram_title  = $instagram['title'] @endphp
-                    @php $instagram_target = $instagram['target'] @endphp @php '_self' @endphp
+                @if($instagram)
+                    <li>
+                        @php $instagram_url  = $instagram['url'] @endphp
+                        @php $instagram_title  = $instagram['title'] @endphp
+                        @php $instagram_target = $instagram['target'] @endphp @php '_self' @endphp
 
-                    <a href="@php echo esc_url( $instagram_url ) @endphp" target="@php echo esc_attr( $instagram_target ) @endphp"><i class="fab fa-instagram"></i> <span class="sr-only">@php echo esc_html( $instagram_title ) @endphp</span></a>
-                </li>
-            @endif
+                        <a href="@php echo esc_url( $instagram_url ) @endphp" target="@php echo esc_attr( $instagram_target ) @endphp"><i class="fab fa-instagram"></i> <span class="sr-only">@php echo esc_html( $instagram_title ) @endphp</span></a>
+                    </li>
+                @endif
 
-            @if($twitter)
-                <li>
-                    @php $twitter_url  = $twitter['url'] @endphp
-                    @php $twitter_title  = $twitter['title'] @endphp
-                    @php $twitter_target = $twitter['target'] @endphp @php '_self' @endphp
+                @if($twitter)
+                    <li>
+                        @php $twitter_url  = $twitter['url'] @endphp
+                        @php $twitter_title  = $twitter['title'] @endphp
+                        @php $twitter_target = $twitter['target'] @endphp @php '_self' @endphp
 
-                    <a href="@php echo esc_url( $twitter_url ) @endphp" target="@php echo esc_attr( $twitter_target ) @endphp"><i class="fab fa-twitter"></i> <span class="sr-only">@php echo esc_html( $twitter_title ) @endphp</span></a>
-                </li>
-            @endif
-        </ul>
+                        <a href="@php echo esc_url( $twitter_url ) @endphp" target="@php echo esc_attr( $twitter_target ) @endphp"><i class="fab fa-twitter"></i> <span class="sr-only">@php echo esc_html( $twitter_title ) @endphp</span></a>
+                    </li>
+                @endif
+            </ul>
+          </div>
+        </div>
     </div>
 </div>
