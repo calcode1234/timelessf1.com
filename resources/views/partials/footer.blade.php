19591c6d64797610e2699579da9f12f78d0468d3
Callum Gamble <callum@kreativeincagency.co.uk>
2022-07-06 14:51:55 +0100

    footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,7 @@
 <div id="site-footer" role="contentinfo" class="section">
-    @php wp_nav_menu(array( 'menu' => 'Footer Menu' ) ) @endphp
+    <div class="container">
+        @php wp_nav_menu(array( 'menu' => 'Footer Menu' ) ) @endphp
 
-    <p>@php echo esc_html('All Rights Reserved at Timeless F1') @endphp</p>
+        <p>@php echo esc_html('All Rights Reserved at Timeless F1') @endphp</p>
+    </div>
 </div>
