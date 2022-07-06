83333e4edb5947bd76d5452535620c86f7947965
Callum Gamble <callum@kreativeincagency.co.uk>
2022-07-06 14:54:01 +0100

    fix for footer styling issue

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,11 @@
 <div id="site-footer" role="contentinfo" class="section">
     <div class="container">
         @php wp_nav_menu(array( 'menu' => 'Footer Menu' ) ) @endphp
+    </div>
+</div>
 
-        <p>@php echo esc_html('All Rights Reserved at Timeless F1') @endphp</p>
+<div class="bg-secondary section">
+    <div class="container">
+        <p class="mb-0 text-center text-white">@php echo esc_html('All Rights Reserved at Timeless F1') @endphp</p>
     </div>
 </div>
