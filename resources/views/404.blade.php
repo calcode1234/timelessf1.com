902af0999b2e6aea061e519e3486854fc4b122b5
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-01 20:26:29 +0100

    CSS styling for about page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,30 +1,253 @@
-@extends('layouts.app')
+@if(is_front_page())
+    <section id="latest" class="bg-light">
+        <div class="col-lg-10 col-12 mx-auto py-lg-5 py-3">
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto">
+                    @include('partials.searchform')
+                </div>
+            </div>
+            <div class="row">
+                @php $args = array(
+                    'post_type' => 'post',
+                    'posts_per_page' => 1
+                ) @endphp
+                    
+                @php $query = new WP_Query( $args ) @endphp
 
-@section('content')
-  <section id="error" class="bg-light">
-    <div class="col-lg-10 col-12 mx-auto py-lg-5 py-3">
-        <div class="row">
-            <div class="col-12 heading">
-                <div class="row">
-                    <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
-                        <h2><strong class="bg-primary text-secondary">Not Found</strong></h2>
-                    </div>
-                    <div class="col-lg-3 col-12">
-                        <a href="@php echo esc_url( home_url( '/' ) ) @endphp" class="btn mb-3" role="button">{{__('Back to homepage') }}</a>
-                    </div>
+                @if($query->have_posts())
+                    @while($query->have_posts())
+                        @php $query->the_post() @endphp
+
+                        <div class="col-lg-8 col-12 py-3">
+                            <article class="grid bg-white py-3 mx-lg-3 mx-0">
+                                <h1 class="h5 pt-3 px-lg-5 px-3 pb-0"><strong class="bg-info text-white">{{__('Article') }}</strong></h1>
+                                <h2 class="h3 text-secondary py-3 px-lg-5 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                @if(has_post_thumbnail())
+                                    @php $attachment_id = get_post_thumbnail_id() @endphp
+                                    @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+                                    <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="100%" height="auto">
+                                @endif
+                                <div class="my-3 mx-lg-5 mx-3"><a href="@php the_permalink() @endphp" class="btn" role="button" aria-label="Read more about the @php the_title() @endphp post">{{__('Read more') }}</a></div>
+                            </article>
+                        </div>
+                    @endwhile
+                @endif
+
+                @php wp_reset_postdata() @endphp
+                
+                <div class="col-lg-4 col-12">
+                    @php $video_args = array(
+                        'post_type' => 'videos',
+                        'posts_per_page' => 1
+                    ) @endphp
+
+                    @php $video_query = new WP_Query( $video_args ) @endphp
+
+                    @if($video_query->have_posts())
+                        @while($video_query->have_posts())
+                            @php $video_query->the_post() @endphp
+
+                            <div class="row">
+                                <div class="col-12 py-3">
+                                    <article class="grid bg-white py-3 mx-3">
+                                        <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-secondary text-white">{{__('Video') }}</strong></h2>
+                                        <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                        @if(has_post_thumbnail())
+                                            @php $attachment_id = get_post_thumbnail_id() @endphp
+                                            @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+                                            <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="100%" height="auto">
+                                        @endif
+                                        <div class="m-3"><a href="@php the_permalink() @endphp" class="btn" role="button" aria-label="Watch the @php the_title() @endphp video">{{__('Watch video') }}</a></div>
+                                    </article>
+                                </div>
+                            </div>
+                        @endwhile
+                    @endif
+
+                    @php wp_reset_postdata() @endphp
+
+                    @php $quiz_args = array(
+                        'post_type' => 'quizzes',
+                        'posts_per_page' => 1
+                    ) @endphp
+
+                    @php $quiz_query = new WP_Query( $quiz_args ) @endphp
+
+                    @if($quiz_query->have_posts())
+                        @while($quiz_query->have_posts())
+                            @php $quiz_query->the_post() @endphp
+
+                            <div class="row">
+                                <div class="col-12 py-3">
+                                    <article class="grid bg-white py-3 mx-3">
+                                        <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-success text-white">{{__('Quiz') }}</strong></h2>
+                                        <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                        @if(has_post_thumbnail())
+                                            @php $attachment_id = get_post_thumbnail_id() @endphp
+                                            @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+                                            <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="100%" height="auto">
+                                        @endif
+                                        <div class="m-3"><a href="@php the_permalink() @endphp" class="btn" role="button" aria-label="Start the @php the_title() @endphp quiz">{{__('Test your knowledge') }}</a></div>
+                                    </article>
+                                </div>
+                            </div>
+                        @endwhile
+                    @endif
+
+                    @php wp_reset_postdata() @endphp
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-10 col-12 mx-auto">
-                <p class="my-4">Sorry, but the article, video, quiz or page you were looking for doesn't exist.</p>
+    </section>
+
+    <section id="articles">
+        <div class="col-lg-10 col-12 mx-auto py-lg-5 py-3">
+            <div class="row">
+                <div class="col-12">
+                    @include('partials.adsense')
+                </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-10 col-12 mx-auto">
-                @include('partials.searchform')
+
+        <div class="bg-light-blue">
+            <div class="col-lg-10 col-12 mx-auto py-lg-5 py-3">
+
+                @if($query->have_posts())
+                    <div class="row">
+                        <div class="col-12 heading">
+                            <div class="row">
+                                <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
+                                    <h2><strong class="bg-info text-white">Articles</strong></h2>
+                                </div>
+                                <div class="col-lg-3 col-12">
+                                    <a href="@php echo get_permalink( get_option( 'page_for_posts' ) ) @endphp" class="btn mb-3" role="button">{{__('Browse all articles') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        @while($query->have_posts())
+                            @php $query->the_post() @endphp
+
+                            <div class="col-lg-4 col-12">
+                                <article class="grid bg-white my-lg-5 my-3 py-3">
+                                    <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-info text-white">{{__('Article') }}</strong></h2>
+                                    <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                    @if(has_post_thumbnail())
+                                        @php $attachment_id = get_post_thumbnail_id() @endphp
+                                        @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+                                        <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="100%" height="auto">
+                                    @endif
+                                    <div class="my-3 mx-3"><a href="@php the_permalink() @endphp" class="btn" role="button" aria-label="Read more about the @php the_title() @endphp post">{{__('Read more') }}</a></div>
+                                </article>
+                            </div>
+                        @endwhile
+                    </div>
+                @endif
             </div>
         </div>
-    </div>
-  </section>
-@endsection
+    </section>
+
+    
+    <section id="videos">
+        <div class="col-lg-10 col-12 mx-auto">
+            <div class="row">
+                <div class="col-12 py-5">
+                    @include('partials.adsense')
+                </div>
+            </div> 
+        </div>
+
+        <div class="bg-light-red">
+            @if($video_query->have_posts())
+                <div class="col-lg-10 col-12 mx-auto py-lg-5 py-3">
+                    <div class="row">
+                        <div class="col-12 heading">
+                            <div class="row">
+                                <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
+                                    <h2><strong class="bg-secondary text-white">Videos</strong></h2>
+                                </div>
+                                <div class="col-lg-3 col-12">
+                                    <a href="@php echo get_post_type_archive_link( 'videos' ) @endphp" class="btn mb-3" role="button">{{__('Browse all videos') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        @while($video_query->have_posts())
+                            @php $video_query->the_post() @endphp
+
+                            <div class="col-lg-4 col-12">
+                                <article class="grid bg-white my-lg-5 my-3 py-3">
+                                    <h2 class="h5 text-danger pt-3 px-lg-4 px-3 pb-0"><strong class="bg-secondary text-white">{{__('Video') }}</strong></h2>
+                                    <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                    @if(has_post_thumbnail())
+                                        @php $attachment_id = get_post_thumbnail_id() @endphp
+                                        @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+                                        <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="100%" height="auto">
+                                    @endif
+                                    <div class="my-3 mx-3"><a href="@php the_permalink() @endphp" class="btn" role="button" aria-label="Watch the @php the_title() @endphp video">{{__('Watch video') }}</a></div>
+                                </article>
+                            </div>
+                        @endwhile
+                    </div>
+                </div>
+            @endif
+        </div>
+
+        @php wp_reset_postdata() @endphp
+    </section>
+
+    <section id="quizzes">
+        <div class="col-lg-10 col-12 mx-auto">
+            <div class="row">
+                <div class="col-12 py-5">
+                    @include('partials.adsense')
+                </div>
+            </div> 
+        </div>
+
+        <div class="bg-light-green">
+            @if($quiz_query->have_posts())
+                <div class="col-lg-10 col-12 mx-auto py-lg-5 py-3">
+                    <div class="row">
+                        <div class="col-12 heading">
+                            <div class="row">
+                                <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
+                                    <h2><strong class="bg-success text-white">Quizzes</strong></h2>
+                                </div>
+                                <div class="col-lg-3 col-12">
+                                    <a href="@php echo get_post_type_archive_link( 'quizzes' ) @endphp" class="btn mb-3" role="button">{{__('Browse all quizzes') }}</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        @while($quiz_query->have_posts())
+                            @php $quiz_query->the_post() @endphp
+
+                            <div class="col-lg-4 col-12">
+                                <article class="grid bg-white my-lg-5 my-3 py-3">
+                                    <h2 class="h5 text-danger pt-3 px-lg-4 px-3 pb-0"><strong class="bg-success text-white">{{__('Quiz') }}</strong></h2>
+                                    <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                    @if(has_post_thumbnail())
+                                        @php $attachment_id = get_post_thumbnail_id() @endphp
+                                        @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+                                        <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="100%" height="auto">
+                                    @endif
+                                    <div class="my-3 mx-3"><a href="@php the_permalink() @endphp" class="btn" role="button" aria-label="Start the @php the_title() @endphp quiz">{{__('Start quiz') }}</a></div>
+                                </article>
+                            </div>
+                        @endwhile
+                    </div>
+                </div>
+            @endif
+        </div>
+
+        @php wp_reset_postdata() @endphp
+    </section>
+    @include('partials.mailchimp')
+@endif
--- resources/views/template-about.blade.php
+++ resources/views/template-about.blade.php
@@ -6,6 +6,89 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    
+    <section id="about" class="bg-light">
+        <div class="col-lg-10 col-12 mx-auto pt-lg-5 pb-lg-3 py-3">
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto">
+                    @include('partials.searchform')
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-8 col-12 mx-auto">
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto heading pb-2 mb-5">
+                    <div class="row">
+                        <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
+                            <h2><strong class="bg-primary text-secondary">About</strong></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto mb-lg-0 mb-3 pl-lg-0 text-dark">
+                    @php the_content() @endphp
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto mb-lg-0 mb-3 pl-lg-0">
+                    <ul class="social mb-0">
+                        @if($tiktok)
+                            <li>
+                                @php $tiktok_url  = $tiktok['url'] @endphp
+                                @php $tiktok_title  = $tiktok['title'] @endphp
+                                @php $tiktok_target = $tiktok['target'] @endphp @php '_self' @endphp
+
+                                <a href="@php echo esc_url( $tiktok_url ) @endphp" target="@php echo esc_attr( $tiktok_target ) @endphp"><i class="fab fa-tiktok"></i> <span class="sr-only">@php echo esc_html( $tiktok_title ) @endphp</span></a>
+                            </li>
+                        @endif
+
+                        @if($youtube)
+                            <li>
+                                @php $youtube_url  = $youtube['url'] @endphp
+                                @php $youtube_title  = $youtube['title'] @endphp
+                                @php $youtube_target = $youtube['target'] @endphp @php '_self' @endphp
+
+                                <a href="@php echo esc_url( $youtube_url ) @endphp" target="@php echo esc_attr( $youtube_target ) @endphp"><i class="fab fa-youtube"></i> <span class="sr-only">@php echo esc_html( $youtube_title ) @endphp</span></a>
+                            </li>
+                        @endif
+
+                        @if($instagram)
+                            <li>
+                                @php $instagram_url  = $instagram['url'] @endphp
+                                @php $instagram_title  = $instagram['title'] @endphp
+                                @php $instagram_target = $instagram['target'] @endphp @php '_self' @endphp
+
+                                <a href="@php echo esc_url( $instagram_url ) @endphp" target="@php echo esc_attr( $instagram_target ) @endphp"><i class="fab fa-instagram"></i> <span class="sr-only">@php echo esc_html( $instagram_title ) @endphp</span></a>
+                            </li>
+                        @endif
+
+                        @if($twitter)
+                            <li>
+                                @php $twitter_url  = $twitter['url'] @endphp
+                                @php $twitter_title  = $twitter['title'] @endphp
+                                @php $twitter_target = $twitter['target'] @endphp @php '_self' @endphp
+
+                                <a href="@php echo esc_url( $twitter_url ) @endphp" target="@php echo esc_attr( $twitter_target ) @endphp"><i class="fab fa-twitter"></i> <span class="sr-only">@php echo esc_html( $twitter_title ) @endphp</span></a>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+
+            @if(has_post_thumbnail())
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto my-3 pl-lg-0 text-center">
+                    @php $attachment_id = get_post_thumbnail_id() @endphp
+                    @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
+
+                    <img src="@php the_post_thumbnail_url() @endphp" alt="@php echo $alt @endphp" width="300px" height="300px">
+                </div>
+            </div>
+            @endif
+        </div>
+    </section>
   @endwhile
 @endsection
