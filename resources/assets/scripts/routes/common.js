8e28a88e952b33d7a8588c40ae2299e25b36ee03
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-03 15:21:57 +0100

    Added JS code for subscribe button

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -4,5 +4,29 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+
+    jQuery('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+      if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname ) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+        if (target.length) {
+          event.preventDefault();
+
+          jQuery('html, body').animate({
+            scrollTop: target.offset().top,
+          }, 500, function() {
+            var $target = jQuery(target);
+            $target.focus();
+            if ($target.is(':focus')) {
+              return false;
+            } else {
+              $target.attr('tabindex','-1');
+              $target.focus();
+            }
+          });
+        }
+      }
+    });
   },
 };
