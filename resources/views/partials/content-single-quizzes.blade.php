206a1738d637bfeab50ce05b0cb2347d1daae4aa
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-03 15:11:36 +0100

    Added related content to single quiz pages

--- resources/views/partials/content-single-quizzes.blade.php
+++ resources/views/partials/content-single-quizzes.blade.php
@@ -29,6 +29,44 @@
                 @php the_content() @endphp
             </div>
         </div>
+
+        @if(have_rows('related_quizzes'))
+            <div class="row">
+                <div class="col-lg-10 col-12 mx-auto heading pb-2">
+                    <div class="row">
+                        <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
+                            <h2 class="h3"><strong class="bg-success text-white">Related videos</strong></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-10 mx-auto pl-0">
+                    <div class="row">
+                        @while(have_rows('related_quizzes'))
+                            @php the_row() @endphp
+                            @php $quiz = get_sub_field('quiz') @endphp
+                            @if( $quiz )
+                                @php $featured_image = get_the_post_thumbnail_url($quiz->ID) @endphp
+
+                                <div class="col-lg-4 col-12">
+                                    <article class="grid bg-white my-lg-5 my-3 py-3">
+                                        <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-success text-white">{{__('Quiz') }}</strong></h2>
+                                        <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $quiz->post_title ) @endphp</strong></h2>
+                                        @if(has_post_thumbnail())
+                                            @php $featured_image = get_the_post_thumbnail_url($quiz->ID) @endphp
+                                            <img src="@php echo $featured_image @endphp" alt="@php echo get_post_meta( get_post_thumbnail_id($quiz->ID), '_wp_attachment_image_alt', true ) @endphp" width="100%" height="auto">
+                                        @endif
+                                        <div class="my-3 mx-3"><a href="@php the_permalink($quiz->ID) @endphp" class="btn" role="button" aria-label="Start the @php echo esc_html( $quiz->post_title ) @endphp quiz">{{__('Start quiz') }}</a></div>
+                                    </article>
+                                </div>
+                            @endif
+                        @endwhile
+                            </div>
+                        </div>
+                    </div>
+        @endif
     </div>
 </section>
 
