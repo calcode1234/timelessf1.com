b4b46f7bc1be6b7a2183de7f3d0daa7214837828
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-05 15:54:39 +0100

    Added JS for keyboard accessible checkboxes

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -5,6 +5,11 @@ export default {
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
 
+    $('html.ie').removeClass('no-js').addClass('js');
+		$('#nojs-notice').remove();
+		$('noscript').remove();
+    $('.navbar li a').removeAttr('title');
+
     jQuery('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
       if ( location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname ) {
         var target = $(this.hash);
@@ -27,6 +32,11 @@ export default {
           });
         }
       }
+    });
+
+    $('[type=checkbox], [type=radio]').on('keypress', function(e){
+      e.preventDefault();
+      $(this).trigger('click');
     });
   },
 };
