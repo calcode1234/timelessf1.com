aaad2ab5ad01b7d1e8e735459956e56b35c154e7
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-05 15:41:25 +0100

    Changed search bar id

--- resources/views/partials/searchform.blade.php
+++ resources/views/partials/searchform.blade.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="search-form" action="@php echo esc_url( home_url( '/' ) ) @endphp" class="input-group mb-3 mx-lg-3 mx-0">
   <div class="input-group">
-    <input type="search" class="form-control border-0" placeholder="Search Timeless F1…" aria-label="search nico" name="s" id="search-input" value="@php echo esc_attr( get_search_query() ) @endphp">
+    <input type="search" class="form-control border-0" placeholder="Search Timeless F1…" aria-label="search nico" name="s" id="content" value="@php echo esc_attr( get_search_query() ) @endphp">
     <div class="input-group-append">
       <span class="input-group-text p-0">
         <i class="fas fa-search text-muted"></i>
