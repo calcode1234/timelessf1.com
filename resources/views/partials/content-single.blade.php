51496ba28c94314449d04faea4591080ea910ed7
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-20 13:55:45 +0100

    Colour change fixes for the single articles pages

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -88,7 +88,7 @@
                         <div class="col-lg-10 col-11 mx-auto heading pb-2">
                             <div class="row">
                                 <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
-                                    <h2 class="h3"><strong class="bg-info text-white">Related articles</strong></h2>
+                                    <h2 class="h3"><strong class="bg-danger text-white">Related articles</strong></h2>
                                 </div>
                             </div>
                         </div>
@@ -105,8 +105,8 @@
 
                                         <div class="col-lg-4 col-sm-6 col-12 d-sm-flex flex-sm-column">
                                             <article class="grid bg-white my-lg-5 my-3 py-3 flex-sm-grow-1">
-                                                <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-info text-white">{{__('Article') }}</strong></h2>
-                                                <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $article->post_title ) @endphp</strong></h2>
+                                                <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-danger text-white">{{__('Article') }}</strong></h2>
+                                                <h2 class="h4 text-primary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $article->post_title ) @endphp</strong></h2>
                                                 @if(has_post_thumbnail())
                                                     @php $featured_image = get_the_post_thumbnail_url($article->ID) @endphp
                                                     <img src="@php echo $featured_image @endphp" alt="@php echo get_post_meta( get_post_thumbnail_id($article->ID), '_wp_attachment_image_alt', true ) @endphp" width="100%" height="auto">
