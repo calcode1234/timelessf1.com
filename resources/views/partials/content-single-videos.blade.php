53a4e1c3eb8ff073f80cef2ca45b9eb07766758f
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-20 14:06:04 +0100

    Colour changes to single videos post

--- resources/views/partials/content-single-videos.blade.php
+++ resources/views/partials/content-single-videos.blade.php
@@ -1,4 +1,4 @@
-<section class="page video bg-light-red">
+<section class="page video bg-danger">
     <div class="col-lg-10 col-12 mx-auto pt-lg-5 pb-lg-3 py-3">
         <div class="row">
             <div class="col-lg-10 col-12 mx-auto">
@@ -12,7 +12,7 @@
             <div class="col-lg-10 col-11 mx-auto heading pb-2 mb-5">
                 <div class="row">
                     <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
-                        <h1 class="h2"><strong class="bg-secondary text-white">Video</strong></h1>
+                        <h1 class="h2"><strong class="bg-white text-danger">Video</strong></h1>
                     </div>
                 </div>
             </div>
@@ -20,7 +20,7 @@
 
         <div class="row">
             <div class="col-10 mx-auto pl-0 mb-5">
-                <h2 class="text-dark"><strong>@php the_title() @endphp</strong></h2>
+                <h2 class="text-white"><strong>@php the_title() @endphp</strong></h2>
             </div>
         </div>
 
@@ -36,7 +36,7 @@
                 </div>
 
                 <div class="row">
-                    <div class="col-10 mx-auto pl-0 mb-5 text-dark">
+                    <div class="col-10 mx-auto pl-0 mb-5 text-white">
                         @php the_sub_field('excerpt') @endphp
                     </div>
                 </div>
@@ -46,7 +46,7 @@
                         <div class="col-lg-10 col-11 mx-auto heading pb-2">
                             <div class="row">
                                 <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
-                                    <h2 class="h3"><strong class="bg-secondary text-white">Related videos</strong></h2>
+                                    <h2 class="h3"><strong class="bg-danger text-white">Related videos</strong></h2>
                                 </div>
                             </div>
                         </div>
@@ -63,8 +63,8 @@
 
                                         <div class="col-lg-4 col-sm-6 col-12 d-sm-flex flex-sm-column">
                                             <article class="grid bg-white my-lg-5 my-3 py-3 flex-sm-grow-1">
-                                                <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-secondary text-white">{{__('Video') }}</strong></h2>
-                                                <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $video->post_title ) @endphp</strong></h2>
+                                                <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-danger text-white">{{__('Video') }}</strong></h2>
+                                                <h2 class="h4 text-primary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $video->post_title ) @endphp</strong></h2>
                                                 @if(has_post_thumbnail())
                                                     @php $featured_image = get_the_post_thumbnail_url($video->ID) @endphp
                                                     <img src="@php echo $featured_image @endphp" alt="@php echo get_post_meta( get_post_thumbnail_id($video->ID), '_wp_attachment_image_alt', true ) @endphp" width="100%" height="auto">
