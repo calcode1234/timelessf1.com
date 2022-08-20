1fd723f9097726d12d02089acf88bea26a0e36fb
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-20 14:18:33 +0100

    Colour changes on single quizzes post

--- resources/views/partials/content-single-quizzes.blade.php
+++ resources/views/partials/content-single-quizzes.blade.php
@@ -1,4 +1,4 @@
-<article class="page quiz bg-light-green">
+<article class="page quiz">
     <div class="col-lg-10 col-12 mx-auto pt-lg-5 pb-lg-3 py-3">
         <div class="row">
             <div class="col-lg-10 col-12 mx-auto">
@@ -12,7 +12,7 @@
             <div class="col-lg-10 col-11 mx-auto heading pb-2 mb-5">
                 <div class="row">
                     <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
-                        <h1 class="h2"><strong class="bg-success text-white">Quiz</strong></h1>
+                        <h1 class="h2"><strong class="bg-danger text-white">Quiz</strong></h1>
                     </div>
                 </div>
             </div>
@@ -20,12 +20,12 @@
 
         <div class="row">
             <div class="col-10 mx-auto pl-0 mb-5">
-                <h2 class="text-dark"><strong>@php the_title() @endphp</strong></h2>
+                <h2 class="text-primary"><strong>@php the_title() @endphp</strong></h2>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-lg-10 col-11 mx-auto mb-5 pl-lg-0 text-dark">
+            <div class="col-lg-10 col-11 mx-auto mb-5 pl-lg-0 text-primary">
                 @php the_content() @endphp
             </div>
         </div>
@@ -35,7 +35,7 @@
                 <div class="col-lg-10 col-11 mx-auto heading pb-2">
                     <div class="row">
                         <div class="col-lg-9 col-11 mb-lg-0 mb-3 pl-lg-0">
-                            <h2 class="h3"><strong class="bg-success text-white">Related videos</strong></h2>
+                            <h2 class="h3"><strong class="bg-danger text-white">Related quizzes</strong></h2>
                         </div>
                     </div>
                 </div>
@@ -51,9 +51,9 @@
                                 @php $featured_image = get_the_post_thumbnail_url($quiz->ID) @endphp
 
                                 <div class="col-lg-4 col-sm-6 col-12 d-sm-flex flex-sm-column">
-                                    <article class="grid bg-white my-lg-5 my-3 py-3 flex-sm-grow-1">
-                                        <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-success text-white">{{__('Quiz') }}</strong></h2>
-                                        <h2 class="h4 text-secondary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $quiz->post_title ) @endphp</strong></h2>
+                                    <article class="grid bg-light my-lg-5 my-3 py-3 flex-sm-grow-1">
+                                        <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-danger text-white">{{__('Quiz') }}</strong></h2>
+                                        <h2 class="h4 text-primary py-3 px-lg-4 px-3"><strong class="pl-0">@php echo esc_html( $quiz->post_title ) @endphp</strong></h2>
                                         @if(has_post_thumbnail())
                                             @php $featured_image = get_the_post_thumbnail_url($quiz->ID) @endphp
                                             <img src="@php echo $featured_image @endphp" alt="@php echo get_post_meta( get_post_thumbnail_id($quiz->ID), '_wp_attachment_image_alt', true ) @endphp" width="100%" height="auto">
