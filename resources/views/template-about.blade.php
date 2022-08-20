b79b73af5c96c6718def2643db3f3dc26ce46d58
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-20 10:33:52 +0100

    Colour changes for the about page

--- resources/views/template-about.blade.php
+++ resources/views/template-about.blade.php
@@ -20,14 +20,14 @@
                 <div class="col-lg-10 col-12 mx-auto heading pb-2 mb-5">
                     <div class="row">
                         <div class="col-lg-9 col-12 mb-lg-0 mb-3 pl-lg-0">
-                            <h1 class="h2"><strong class="bg-primary text-secondary">About</strong></h1>
+                            <h1 class="h2"><strong class="bg-danger text-white">About</strong></h1>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-lg-10 col-12 mx-auto mb-lg-0 mb-3 pl-lg-0 text-dark">
+                <div class="col-lg-10 col-12 mx-auto mb-lg-0 mb-3 pl-lg-0 text-primary">
                     @php the_content() @endphp
                 </div>
             </div>
