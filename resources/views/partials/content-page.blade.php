fe2385228a5cf0ff293fdc9cad5979e5c2a0f45f
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-20 08:54:42 +0100

    Fixed colour issues on homepage

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -256,7 +256,7 @@
                             <div class="col-md-4 col-sm-6 col-12 d-sm-flex flex-sm-column">
                                 <article class="grid bg-light my-lg-5 my-3 py-3 flex-sm-grow-1">
                                     <h2 class="h5 pt-3 px-lg-4 px-3 pb-0"><strong class="bg-danger text-white">{{__('Quiz') }}</strong></h2>
-                                    <h2 class="h4 text-white py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
+                                    <h2 class="h4 text-primary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
                                     @if(has_post_thumbnail())
                                         @php $attachment_id = get_post_thumbnail_id() @endphp
                                         @php $alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) @endphp
