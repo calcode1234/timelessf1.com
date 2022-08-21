03fe0c4b0ff9ba80ebbb1f2b78a9c725324901cc
Callum Gamble <callum@kreativeincagency.co.uk>
2022-08-21 15:49:10 +0100

    Colour changes for quiz pages 2

--- resources/views/partials/content-quizzes.blade.php
+++ resources/views/partials/content-quizzes.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-4 col-sm-6 col-12 d-sm-flex flex-sm-column">
-    <article class="grid bg-light my-lg-5 my-3 py-3 flex-sm-grow-1">
+    <article class="grid bg-white my-lg-5 my-3 py-3 flex-sm-grow-1">
         <h2 class="h5 text-danger pt-3 px-lg-4 px-3 pb-0"><strong class="bg-danger text-white">{{__('Quiz') }}</strong></h2>
         <h2 class="h4 text-primary py-3 px-lg-4 px-3"><strong class="pl-0">@php the_title() @endphp</strong></h2>
         @if(has_post_thumbnail())
